7508e7d8f9af434cfc9b8f23bcc527e46c7ce10d
RITAM BASU <asystematichaos@gmail.com>
2023-02-28 00:42:04 +0000

    Added variable.tf at root

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+    type = string
+    description = "This is the RG name"
+}
+
+variable "location" {
+  type = string
+  description = "This is the resource location"
+}
